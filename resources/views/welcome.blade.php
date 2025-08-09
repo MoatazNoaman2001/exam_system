674674368dab2945bdbbe19964d389024e532914
MoatazNoaman <moataz.noaman12@gmail.com>
2025-08-09 18:07:54 +0300

    edit name

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -3,7 +3,7 @@
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>{{ __('lang.PMP Master - Your Complete PMP Exam Preparation System') }}</title>
+    <title>{{ config('app.name', 'Sprint Skills') }}</title>
 
     <link rel="shortcut icon" href="{{asset('images/Sprint_Skills.ico')}}" type="image/x-icon"> 
     <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.4.0/css/all.min.css">
